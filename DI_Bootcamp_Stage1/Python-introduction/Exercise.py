cb4c89647b01b1c1132dbab54731a3731711a3b9
David-Yarden <david.yarden1803@gmail.com>
2025-07-19 14:56:29 +0300

    finished exercise pytthon 1

---
+++ DI_Bootcamp_Stage1/Python-introduction/Exercise.py
@@ -0,0 +1,38 @@
+first = "Hello World"
+print(first)
+#This is a comment
+print("I AM A COMPUTER!")
+if 1<2 and 4>2:
+    print("Math is fun.")
+nope = None
+print(nope)
+result = True and False
+print(result)
+length = "What's my length?"
+textlength = len(length)
+print(textlength)
+text = "i am shouting"
+uppercase_text = text.upper()
+print(uppercase_text)
+string = "1000"
+print(int(string))
+real="real"
+print(f"4{real}")
+# print(3*cool)NameError: name 'cool' is not defined. Did you mean: 'bool'?
+# div0=1/0  div0=1/0
+        #        ~^~
+print(type([]))
+name = input("what is your name?")
+number = int(input("pick a number"))
+if number < 0:
+    print("That number is less than 0!")
+elif number > 0:
+    print("That number is greater than 0!")
+else:
+    print("You picked 0!")
+word = "apple"
+index = word.index("l")
+print(index)
+print("y" in "xylophone")
+my_string = "hello"
+print(my_string.islower())
