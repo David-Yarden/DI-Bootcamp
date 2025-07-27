a3ce8e26c462dc5b490baa18297175011d02b21b
David-Yarden <david.yarden1803@gmail.com>
2025-07-27 19:55:01 +0300

    day 1 week 2

---
+++ Repositories/DI-Bootcamp/week2/Day1/functions.py
@@ -0,0 +1,64 @@
+# def hello():
+#     print('hello there')
+    
+# hello()
+
+# Passing arguments to the function
+
+# def greetings_adv(language = "EN", name = "John"):
+#     if language == "PT":
+#         return f"Ola {name}, tudo bem?"
+#     elif language == "ES":
+#         return f"Hola {name}, que tal"
+#     elif language == "EN":
+#         return f"Hi {name}, how are you?"
+#     else:
+#         return "Unknown language"
+
+# greetings_adv("PT", "David")
+
+# def calculation(num1,num2):
+    # result = num1 + num2
+    # return result
+
+# def multiply(calc):
+#     result = calc * 5
+#     return result
+
+# calc = calculation(5,3)
+# print(multiply(calc))
+
+def country_info(country)-> str:
+    # returns the capital of a given country
+    if country == "Naboo":
+        return "Theed"
+    elif country == "France":
+        return "Paris"
+    elif country == "Israel":
+        return "Jerusalem"
+    else:
+        return "Unknown country"
+
+print(country_info(input("Chose a country: ")))
+
+
+
+#Global and local scope
+
+age = 25
+
+def current_age():
+    print(age)
+    my_age = 35
+    my_age += 1
+
+my_age= 44
+print(my_age)
+
+current_age()
+
+def happy_birthday():
+    age += 1
+    return age
+
+happy_birthday()
---
+++ Repositories/TestFolder/Python-tests/Hideo_Decoder.py
@@ -0,0 +1,10 @@
+hideo_translator = input("Copy paste what hideo said: ")
+
+hideo_translator = (
+    hideo_translator
+    .replace("fuck", "have a wonderful day")
+    .replace("bish", "pal")
+    .replace("bitch", "buddy")
+)
+
+print(hideo_translator)
