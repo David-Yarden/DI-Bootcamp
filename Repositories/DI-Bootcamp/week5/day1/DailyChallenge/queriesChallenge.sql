2526ce052bfa5e3d3fa21d0518ca91aba0be2bb1
David-Yarden <david.yarden1803@gmail.com>
2025-08-18 12:52:17 +0300

    Daily challenge done

---
+++ Repositories/DI-Bootcamp/week5/day1/DailyChallenge/queriesChallenge.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(*) AS actors_count
+FROM Actors;
+
+INSERT INTO actors (first_name, last_name, birthdate, number_oscars)
+VALUES ('John'); --There is an error to signify that INSERT column has more targets than expressions
+
+
