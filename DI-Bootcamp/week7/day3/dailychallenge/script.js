15fdd0dbeb9d93e0c7a0a9fc421c3c6654763220
David-Yarden <david.yarden1803@gmail.com>
2025-09-08 14:23:01 +0300

    daily challenge week 7day3

---
+++ DI-Bootcamp/week7/day3/dailychallenge/script.js
@@ -0,0 +1,9 @@
+const sentence = "The movie is not that bad, I like it";
+const wordNot = sentence.indexOf("not");
+const wordBad = sentence.indexOf("bad");
+if (wordBad > wordNot && wordNot !== -1) {
+  const newSentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+  console.log(newSentence);
+} else {
+  console.log(sentence);
+}
