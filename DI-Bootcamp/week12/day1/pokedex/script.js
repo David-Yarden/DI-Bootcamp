3af63193566c64abca69244e75d20424a3f2fce6
David-Yarden <david.yarden1803@gmail.com>
2025-10-16 18:57:40 +0300

    pokedex started

---
+++ DI-Bootcamp/week12/day1/pokedex/script.js
@@ -0,0 +1,6 @@
+addEventListener('DOMContentLoaded'), () => {
+    let currentPokemonId = 1;}
+const pokedex = document.getElementById('pokedex');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+const randomButton = document.getElementById('random');
